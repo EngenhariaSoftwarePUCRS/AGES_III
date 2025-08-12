367e52b08612034a8929bae37cd5a7a5bb169f7d
Felipe Freitas Silva <developer@felipefs.dev>
2025-08-12 05:34:28 -0300

    Mock Frontend - Fix Deployed URL

--- mock-frontend/vite.config.ts
+++ mock-frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'AGES_III/mock-frontend/',
   plugins: [react(), VitePWA({
     devOptions: { enabled: true },
     manifest: {
