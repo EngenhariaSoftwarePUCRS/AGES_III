075c4ae5ecdd0c4332e147e3fe759a83ad9303f9
Felipe Freitas Silva <developer@felipefs.dev>
2025-08-12 02:39:24 -0300

    Mock Frontend - TanStack Query
    Mock API fetching

--- mock-frontend/src/main.tsx
+++ mock-frontend/src/main.tsx
@@ -2,18 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App.tsx'
 import About from './pages/About.tsx'
 
 const root = document.getElementById("root");
+const queryClient = new QueryClient();
 
 createRoot(root!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="about/:count" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="about/:count" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
--- mock-frontend/src/pages/About.tsx
+++ mock-frontend/src/pages/About.tsx
@@ -1,16 +1,48 @@
 import { Link, useParams, useSearchParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
 
 function About() {
     const { count } = useParams();
     const [searchParams] = useSearchParams();
     const c2 = searchParams.get("c2");
 
+    const { isPending, error, data, isFetching } = useQuery({
+      queryKey: ['repoData'],
+      queryFn: async () => {
+        const response = await fetch(
+          'https://api.github.com/repos/tanstack/query'
+        )
+        return await response.json()
+      }
+    })
+
+    if (isPending) {
+      return 'Loading...'
+    }
+
+    if (error) {
+      return (
+        <p>
+          Error: {error.message}
+        </p>
+      )
+    }
+  
   return (
     <div>
       <h1>About</h1>
       <p>Path param count: {count}</p>
       <p>Search param 'c2': {c2}</p>
       <Link to={"/"}>App</Link>
+
+      <div>
+        <h1>{data.full_name}</h1>
+        <p>{data.description}</p>
+        <strong>👀 {data.subscribers_count}</strong>{' '}
+        <strong>✨ {data.stargazers_count}</strong>{' '}
+        <strong>🍴 {data.forks_count}</strong>
+        <div>{isFetching ? 'Updating...' : ''}</div>
+      </div>
     </div>
   )
 }
