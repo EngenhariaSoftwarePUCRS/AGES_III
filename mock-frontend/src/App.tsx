d8373cb14acedb92dc502e709589ed5376594507
Felipe Freitas Silva <developer@felipefs.dev>
2025-08-12 04:52:23 -0300

    Mock Frontend - PWA
    And many icons

--- mock-frontend/src/App.tsx
+++ mock-frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router';
 
 import './App.scss';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,7 +12,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={'/vite.svg'} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
--- mock-frontend/vite.config.ts
+++ mock-frontend/vite.config.ts
@@ -1,9 +1,44 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    devOptions: { enabled: true },
+    manifest: {
+      name: 'WimBelemDon MAIS',
+      short_name: 'WBD+',
+      description: 'An app from WimBelemDon MAIS',
+      theme_color: '#8DC740',
+      background_color: '#0E6872',
+      start_url: '.',
+      display: 'standalone',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    registerType: 'autoUpdate',
+  })],
   server: {
     port: 5173,
     host: '0.0.0.0',
